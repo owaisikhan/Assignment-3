f027a8c358c3e871726a50c97524c91c65055ce4
Owais Khan <owaiskhandawar310@gmail.com>
2020-06-09 20:06:41 +0500

    updated yml

--- src/App.js
+++ src/App.js
@@ -3,8 +3,11 @@ import Form from './Form'
 function App(){
     return(
         <div>
+            <h1>Developer Form</h1>
             <Form account="Facebook ID" name="facebook email"/>
+            <br/>
             <Form account="Github ID" name="github email"/>
+            <br/>
             <Form account="Fiverr" name="fiverr email"/>
         </div>
 
